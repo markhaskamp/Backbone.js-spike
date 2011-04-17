8d20bedafb13631b137e5b9e1b9632ece7d50724
Mark Haskamp <mrhask@gmail.com>
2011-04-17 11:39:07 -0400

    refactor

--- app/lib/PageView.js
+++ app/lib/PageView.js
@@ -9,12 +9,25 @@ var PageView = Backbone.View.extend({
     },
 
   render: function() {
+    this.paint_html();
+    this.setup_drag_and_drop();
+  },
+
+  get_droppable_team: function() {
+    return ($('.dragged_over').html());
+  },
+  
+  paint_html: function() {
     var html_str = "";
+
     _.each(this.model.get('team_list'), function(team) {
       html_str += '\n<div class="team">' + team + '</div>';
     });
 
     $('div#teams_container').html(html_str);
+  },
+
+  setup_drag_and_drop: function() {
     $('.team').draggable({
       revert: 'invalid',
       opacity: '0.50',
@@ -22,22 +35,6 @@ var PageView = Backbone.View.extend({
     });
 
     $('.team').droppable({ hoverClass: 'dragged_over' });
-  },
-
-  handle_team_drop: function(draggable_team_html) {
-    droppable_team = PageView.get_droppable_team();
-    draggable_index = TeamList.get_index_for_team(draggable_team_html);
-    droppable_index = TeamList.get_index_for_team(droppable_team);
-
-    if (draggable_index !== null || droppable_index !== null) {
-      TeamList.reorder_teams(draggable_index, droppable_index);
-    }
-
-    ea.publish(event_key.RENDER_TEAMS);
-  },
-
-  get_droppable_team: function() {
-    return ($('.dragged_over').html());
-  },
+  }
 
 });
