f46c00928de5c309e1740a31b941a2a8e418cc27
Mark Haskamp <mrhask@gmail.com>
2011-04-18 20:18:22 -0400

    mrh

--- test/jasmine/src/TeamList.js
+++ test/jasmine/src/TeamList.js
@@ -1,16 +1,18 @@
-var TeamList = {
-  team_list: ["Chicago",
-              "Cincinnati",
-              "Houston",
-              "Milwaukee",
-              "Pittsburgh",
-              "St. Louis"],
+var TeamList = Backbone.Model.extend({
+  initialize: function() {
+    this.set({'team_list': ["Chicago",
+                            "Cincinnati",
+                            "Houston",
+                            "Milwaukee",
+                            "Pittsburgh",
+                            "St. Louis"]});
+  },
 
   get_index_for_team: function(team_html) {
     team_html = $.trim(team_html);
 
-    for(i=0; i<TeamList.team_list.length; i++) {
-      if ($.trim(TeamList.team_list[i]) === team_html) {
+    for(i=0; i<this.get('team_list').length; i++) {
+      if ($.trim(this.get('team_list')[i]) === team_html) {
         return(i);
       }
     }
@@ -22,18 +24,29 @@ var TeamList = {
     new_list = [];
 
     for (i=0; i<under_ndx; i++) {
-      new_list.push(TeamList.team_list[i]);
+      new_list.push(this.get('team_list')[i]);
     }
-    new_list.push(TeamList.team_list[over_ndx]);
+    new_list.push(this.get('team_list')[over_ndx]);
 
     for (i=under_ndx+1; i<=over_ndx; i++) {
-      new_list[i] = TeamList.team_list[i-1];
+      new_list[i] = this.get('team_list')[i-1];
     }
 
-    for (i=over_ndx+1; i<TeamList.team_list.length; i++) {
-      new_list.push(TeamList.team_list[i]);
+    for (i=over_ndx+1; i<this.get('team_list').length; i++) {
+      new_list.push(this.get('team_list')[i]);
     }
 
-    TeamList.team_list = new_list;
+    this.set({"team_list": new_list});
+  },
+
+  handle_team_drop: function(draggable_team_html, droppable_teM) {
+    draggable_index = this.get_index_for_team(draggable_team_html);
+    droppable_index = this.get_index_for_team(droppable_team);
+
+    if (draggable_index !== null || droppable_index !== null) {
+      this.reorder_teams(draggable_index, droppable_index);
+      this.change();
+    }
+
   }
-};
+});
